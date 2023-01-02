08a824eea4842f93f8dfcaba339e5032da59405b
Venugopal <venugopala@arzooo.com>
2023-01-02 10:25:59 +0530

    HCA-21 - Dashboard - Redux Setup.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import OurProcess from "./pages/OurProcess";
@@ -12,25 +14,29 @@ import UserDashboard from "./pages/UserDashboard";
 import CaDashboard from "./pages/CaDashboard";
 import Dashboard from "./pages/Dashboard";
 
+import store from "./store";
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/our-process" element={<OurProcess />} />
-        <Route path="/get-started" element={<GetStarted />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/user-dashboard" element={<UserDashboard />} />
-        <Route path="/ca-dashboard" element={<CaDashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/our-process" element={<OurProcess />} />
+          <Route path="/get-started" element={<GetStarted />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/user-dashboard" element={<UserDashboard />} />
+          <Route path="/ca-dashboard" element={<CaDashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
