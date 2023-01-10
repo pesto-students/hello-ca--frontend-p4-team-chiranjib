309f31130b5ca78748dac022e0385cf4d74cf062
Venugopal <venugopala@arzooo.com>
2023-01-10 23:30:45 +0530

    Lazy Loading of pages and added Loader as Suspense component.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import Home from "./pages/Homepage";
-import Services from "./pages/Services";
-import OurProcess from "./pages/OurProcess";
-import GetStarted from "./pages/GetStarted";
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
-import Faq from "./pages/Faq";
-import Login from "./pages/Login";
-import CreateAccount from "./pages/CreateAccount";
-import UserDashboard from "./pages/UserDashboard";
-import CaDashboard from "./pages/CaDashboard";
-import Dashboard from "./pages/Dashboard";
+import LoaderWrapper from "./components/Loader";
 
 import store from "./store";
+
+const Home = lazy(() => import("./pages/Homepage"));
+const Services = lazy(() => import("./pages/Services"));
+const OurProcess = lazy(() => import("./pages/OurProcess"));
+const GetStarted = lazy(() => import("./pages/GetStarted"));
+const AboutUs = lazy(() => import("./pages/AboutUs"));
+const ContactUs = lazy(() => import("./pages/ContactUs"));
+const Faq = lazy(() => import("./pages/Faq"));
+const Login = lazy(() => import("./pages/Login"));
+const CreateAccount = lazy(() => import("./pages/CreateAccount"));
+const UserDashboard = lazy(() => import("./pages/UserDashboard"));
+const CaDashboard = lazy(() => import("./pages/CaDashboard"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/our-process" element={<OurProcess />} />
-          <Route path="/get-started" element={<GetStarted />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/create-account" element={<CreateAccount />} />
-          <Route path="/user-dashboard" element={<UserDashboard />} />
-          <Route path="/ca-dashboard" element={<CaDashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
+      <Suspense fallback={<LoaderWrapper />}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/our-process" element={<OurProcess />} />
+            <Route path="/get-started" element={<GetStarted />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/create-account" element={<CreateAccount />} />
+            <Route path="/user-dashboard" element={<UserDashboard />} />
+            <Route path="/ca-dashboard" element={<CaDashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </Suspense>
     </Provider>
   );
 };
