6095473aa96b3892869060b97a6b5805d31e5053
Venugopal <venugopala@arzooo.com>
2023-01-13 23:08:43 +0530

    replaceing mapStateToProps with useSelector.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./style.scss";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import { useSelector } from "react-redux";
 
 import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
@@ -13,7 +12,8 @@ import ProfileCard from "./components/ProfileCard";
 import History from "./components/History";
 import Topics from "./components/Topics";
 
-const Dashboard = (props) => {
+const Dashboard = () => {
+  const user = useSelector((state) => state.user);
   return (
     <Layout>
       <Grid container spacing={2}>
@@ -27,12 +27,12 @@ const Dashboard = (props) => {
             >
               Welcome{" "}
               <b>
-                {props?.user?.data?.first_name} {props?.user?.data?.last_name}
+                {user?.data?.first_name} {user?.data?.last_name}
               </b>
               ,{" "}
-              {props.user?.data?.user_type === "USER"
+              {user?.data?.user_type === "USER"
                 ? "get started with a call"
-                : props.user?.data?.user_type === "CA"
+                : user?.data?.user_type === "CA"
                 ? "enable topics to get calls"
                 : ""}
             </Typography>
@@ -53,19 +53,4 @@ const Dashboard = (props) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(
-    {
-      // getUserDetails: getUserDetails,
-    },
-    dispatch
-  );
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default Dashboard;
